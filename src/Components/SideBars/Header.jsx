1aa46ff84c65706ec1be8b70a486a9566ba8511a
Holiday <araragikyn@gmail.com>
2023-08-04 16:04:01 +0800

    footer header blob

---
+++ src/Components/SideBars/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = (props) => {
+    return(
+        <div>
+            <ul>
+                <li>About</li>
+                <li>Blog</li>
+                <li>Course</li>
+                <li>Lessons</li>
+                <li>Recording</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
